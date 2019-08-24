4081df46c9e249f952f566ae121f2751ba7fcc9d
潘博 <tianzhizi575@hotmail.com>
2019-08-24 14:31:01 +0800

    添加测试api, 添加SonarQube

---
+++ SonarQube.ps1
@@ -0,0 +1,2 @@
+﻿#使用管道进行一步化操作
+dotnet sonarscanner begin /k:"home_vs_anbook" /d:sonar.host.url="http://192.168.148.136:9000" /d:sonar.login="d9507f9ce36cbe24c5874901c8be512256d9854c" | dotnet build | dotnet sonarscanner end /d:sonar.login="d9507f9ce36cbe24c5874901c8be512256d9854c"
